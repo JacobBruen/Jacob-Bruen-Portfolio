b7fc28768dc51d862a796e91b08387b914c323c1
Jacob Bruen <Jrbruen18@gmail.com>
2025-06-19 18:54:04 -0500

    Fix: Add default export to Projects.jsx

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,11 @@
+const Projects = () => {
+    return (
+      <div>
+        <h1>Projects Page</h1>
+        {/* your content */}
+      </div>
+    );
+  };
+  
+  export default Projects;  // <-- This line is necessary!
+  
